c531f911316cd808d2e31de8e5cab996bd823554
psalido@dashlet.info <psalido@dashlet.info@40474b24-9cc2-b64f-8227-0c295578c7c4>
2012-03-21 21:47:23 +0000

    Añade copyright al fichero

--- alfresco-module/source/java/info/dashlet/repository/webscript/PropertiesLoader.java
+++ alfresco-module/source/java/info/dashlet/repository/webscript/PropertiesLoader.java
@@ -1,3 +1,27 @@
+/*
+ *  Content Reports Dashlet for Alfresco (http://www.dashlet.info)
+ *  
+ *  Copyright (C) 2011, 2012 Pedro Salido López <psalido@dashlet.info>
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  NOTICE
+ *  This product includes software used under license from Alfresco Software, Inc., 
+ *  but it is not an Alfresco product and has not been tested, endorsed, or 
+ *  approved by Alfresco Software, Inc. or any of its affiliates.
+ */
+
 package info.dashlet.repository.webscript;
 
 import java.io.InputStream;
